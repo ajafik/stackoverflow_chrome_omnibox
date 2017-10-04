9f5b6be1fd0a1211afa6bf55bceda9ef768d8b54
Oluwafikayo Ajayi <oluwafikayoa@wakanow.com>
2017-10-04 15:58:45 +0100

    Initial Commit

---
+++ background.js
@@ -0,0 +1,26 @@
+function resetDefaultSuggestion() {
+  chrome.omnibox.setDefaultSuggestion({
+    description: 'wakanow.com: Search the WAKANOW.com for %s'
+  });
+}
+
+resetDefaultSuggestion();
+
+chrome.omnibox.onInputChanged.addListener(function(text, suggest) {
+  // Suggestion code will end up here.
+});
+
+chrome.omnibox.onInputCancelled.addListener(function() {
+  resetDefaultSuggestion();
+});
+
+function navigate(url) {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.update(tabs[0].id, {url: url});
+  });
+}
+
+chrome.omnibox.onInputEntered.addListener(function(text) {
+	console.log(text)
+  navigate("https://stackoverflow.com/search?q=" + text);
+});
